bb1d87d0eaff58508f6a1b1c938f207a5739a683
joinsaad <muhammad.saad@venturedive.com>
2020-05-29 21:00:17 +0500

    saad

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -15,26 +15,20 @@ it('it should test the required fields on registering the user on etsy', () => {
     // cy.get("#join_neu_first_name_field").type("saad")
     // cy.get("#join_neu_password_field").type("saad123")
    
-    
 
-
-
-
-    
-   
-
-
-    // cy.get('.new-todo', {timeout: 6000}).type('Clean Room{Enter}')
-    // cy.get('label').should('have.text','Clean Room')
-    // cy.get('toggle').should('not.be.checked')
-
-    // cy.get('.toggle').click()
-    // cy.get('label').should('have.css','text-decoration-line','line-through')
-    // cy.contains('Clear completed').click()
-    // cy.get('todo-list').should('not.have.descendants','li')
-    //some comments
-    //some more comments
 
 })
+
+
+it('it should test the failed login', () => {
+    cy.visit('https://www.etsy.com/')
+    cy.get("button[class$='select-signin']").click()
+    cy.get("#join_neu_email_field").type("joinsaad@gmail.com")
+    cy.get("#join_neu_password_field").type("saad123")  
+    cy.get("button[value='sign-in']").click()
+    cy.get("#aria-join_neu_email_field-error").should("have.text","Email address is invalid.")
+})
+
+
 
 
