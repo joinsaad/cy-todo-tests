1b17de5f6e9df0b6ee477b0ce43243f9af800ff7
joinsaad <muhammad.saad@venturedive.com>
2020-05-29 21:10:15 +0500

    saad

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,10 +1,5 @@
 /// <reference types="cypress"/>
 
-
-
-
-
-
 it('it should test the required fields on registering the user on etsy', () => {
     cy.visit('https://www.etsy.com/')
     cy.get("button[class$='select-signin']").click()
@@ -13,15 +8,8 @@ it('it should test the required fields on registering the user on etsy', () => {
 
     cy.get("#aria-join_neu_email_field-error").should("have.text","Can't be blank.")
     cy.get('#aria-join_neu_first_name_field-error').should("have.text","Can't be blank.")
-    cy.get('#aria-join_neu_password_field-error').should("have.text","Can't be blank.")
-
-
-    // cy.get("#join_neu_email_field").type("joinsaad@gmail.com")
-    // cy.get("#join_neu_first_name_field").type("saad")
-    // cy.get("#join_neu_password_field").type("saad123")
+    cy.get('#aria-join_neu_password_field-error').should("have.text","Can't be blank.")   
    
-
-
 })
 
 
@@ -37,7 +25,6 @@ it('it should test the failed login', () => {
 
 
 const emails = ['joinsaad@gmal.com', 'joinsaad+1@gmail.com','joinsaad+2@gmail.com']
-
 
 emails.forEach((email) => {
 it('data driven it should test the failed login with ${email}`', () => {
